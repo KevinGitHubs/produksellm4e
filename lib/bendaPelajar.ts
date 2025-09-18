efb02b3aca15ba581fbde8499bdc369a8abc3521
KevinGitHubs <103484316+KevinGitHubs@users.noreply.github.com>
2025-09-19 00:24:03 +0700

    Create bendaPelajar.ts

---
+++ lib/bendaPelajar.ts
@@ -0,0 +1,9 @@
+export const BENDA_PELAJAR = [
+  { nama: 'Pensil', emoji: '✏️', coco: 'pencil' },
+  { nama: 'Penggaris', emoji: '📏', coco: 'scissors' },
+  { nama: 'Penghapus', emoji: '🧽', coco: 'eraser' },
+  { nama: 'Buku', emoji: '📕', coco: 'book' },
+  { nama: 'Botol Air', emoji: '🍶', coco: 'bottle' },
+  { nama: 'Tas', emoji: '🎒', coco: 'backpack' },
+  { nama: 'Kipas Angin', emoji: '🌀', coco: 'fan' },
+] as const;
